c999d1d705cbf17378d5543d9a03dd55f02c34f9
sadhna sharnagat <sharsadhna@gmail.com>
2019-05-12 23:58:32 +0530

    home

---
+++ view/src/app/components/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(
+    http: Http
+) { }
+
+  ngOnInit() {
+  }
+
+
+
+}
